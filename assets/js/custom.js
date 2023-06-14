1fb8ba5fdfeb59dc4082cd9984e7df28e27f838b
abhinand9772 <100460106+abhinand9772@users.noreply.github.com>
2023-06-14 11:34:06 +0530

    updt

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -502,6 +502,19 @@ if($('.my-shuffle-container2').length > 0){
 
 /* }(jQuery)); */
 
+/*contact form */
+let form = document.getElementById("comment-form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let full_name = document.getElementById("full-name").value;
+  let email = document.getElementById("email").value;
+  let comments = document.getElementById("comments").value;
+
+  window.location.href = `mailto:victoriapaintindustries@gmail.com?subject=Enquiry from ${full_name} ${email}&body= ${comments}`;
+
+  contact();
+});
+
 
 
 
